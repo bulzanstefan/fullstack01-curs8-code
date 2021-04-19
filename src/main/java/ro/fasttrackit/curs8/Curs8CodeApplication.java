91add751895913ebfbde0348bd741da1e70b61d7
Bulzan Stefan <stefan.bulzan@nordlogic.com>
2021-04-20 00:06:46 +0300

    Initial commit

---
+++ src/main/java/ro/fasttrackit/curs8/Curs8CodeApplication.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.curs8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curs8CodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Curs8CodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/fasttrackit/curs8/Curs8CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.curs8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curs8CodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
